7dd5c8e33965b6d438c04ca20888fe34cc370c05
Gaston Mildenberger <gastonmilden@gmail.com>
2023-11-15 23:12:58 -0300

    PreEntrega2-Mildenberger

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,8 @@ export const products = [
     description: "Descripción Paleta 1",
     price: 10000,
     category: "Paletas gama alta",
-    pictureUrl: "URL de la imagen para la Paleta 1",
+    pictureUrl:
+      "https://prestigio.vteximg.com.br/arquivos/ids/175714-1000-1000/220530_.jpg?v=638241709344070000",
   },
   {
     id: 2,
@@ -13,7 +14,8 @@ export const products = [
     description: "Descripción Paleta 2",
     price: 12000,
     category: "Paletas gama media",
-    pictureUrl: "URL de la imagen para la Paleta 2",
+    pictureUrl:
+      "https://www.stockcenter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw8c00a82d/products/HE_228899/HE_228899-1.JPG",
   },
   {
     id: 3,
@@ -21,7 +23,8 @@ export const products = [
     description: "Descripción Paleta 3",
     price: 9500,
     category: "Paletas gama baja",
-    pictureUrl: "URL de la imagen para la Paleta 3",
+    pictureUrl:
+      "https://sporting.vtexassets.com/arquivos/ids/848900-1200-1200?width=1200&height=1200&aspect=true",
   },
   {
     id: 4,
@@ -29,7 +32,8 @@ export const products = [
     description: "Descripción Paleta 4",
     price: 11000,
     category: "Paletas gama alta",
-    pictureUrl: "URL de la imagen para la Paleta 4",
+    pictureUrl:
+      "https://prestigio.vteximg.com.br/arquivos/ids/175714-1000-1000/220530_.jpg?v=638241709344070000",
   },
   {
     id: 5,
@@ -37,6 +41,7 @@ export const products = [
     description: "Descripción Paleta 5",
     price: 8000,
     category: "Paletas gama media",
-    pictureUrl: "URL de la imagen para la Paleta 5",
+    pictureUrl:
+      "https://www.stockcenter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw8c00a82d/products/HE_228899/HE_228899-1.JPG",
   },
 ];
